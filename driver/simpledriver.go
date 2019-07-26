f702a56d4cdb7cdd5150e3fc5e746b21c7a9d9ac
Enobong Udoko <enobong.n.udoko@intel.com>
2019-07-25 18:00:33 -0700

    initial

---
+++ driver/simpledriver.go
@@ -0,0 +1,143 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2018 Canonical Ltd
+// Copyright (C) 2018-2019 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This package provides a simple example implementation of
+// ProtocolDriver interface.
+//
+package driver
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"math/rand"
+	"os"
+	"time"
+
+	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
+)
+
+const (
+	defMinInt8, defMaxInt8   = -128, 127
+	defMinInt32, defMaxInt32 = -2147483648, 2147483647
+)
+
+type SimpleDriver struct {
+	lc           logger.LoggingClient
+	asyncCh      chan<- *dsModels.AsyncValues
+	switchButton bool
+}
+
+func getImageBytes(imgFile string, buf *bytes.Buffer) error {
+	// Read existing image from file
+	img, err := os.Open(imgFile)
+	if err != nil {
+		return err
+	}
+	defer img.Close()
+
+	// TODO: Attach MediaType property, determine if decoding
+	//  early is required (to optimize edge processing)
+
+	// Expect "png" or "jpeg" image type
+	imageData, imageType, err := image.Decode(img)
+	if err != nil {
+		return err
+	}
+	// Finished with file. Reset file pointer
+	img.Seek(0, 0)
+	if imageType == "jpeg" {
+		err = jpeg.Encode(buf, imageData, nil)
+		if err != nil {
+			return err
+		}
+	} else if imageType == "png" {
+		err = png.Encode(buf, imageData)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Initialize performs protocol-specific initialization for the device
+// service.
+func (s *SimpleDriver) Initialize(lc logger.LoggingClient, asyncCh chan<- *dsModels.AsyncValues) error {
+	s.lc = lc
+	s.asyncCh = asyncCh
+	return nil
+}
+
+// HandleReadCommands triggers a protocol Read operation for the specified device.
+func (s *SimpleDriver) HandleReadCommands(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest) (res []*dsModels.CommandValue, err error) {
+
+	if len(reqs) != 1 {
+		err = fmt.Errorf("SimpleDriver.HandleReadCommands; too many command requests; only one supported")
+		return
+	}
+	s.lc.Debug(fmt.Sprintf("SimpleDriver.HandleReadCommands: protocols: %v resource: %v attributes: %v", protocols, reqs[0].DeviceResourceName, reqs[0].Attributes))
+
+	res = make([]*dsModels.CommandValue, 1)
+	now := time.Now().UnixNano()
+	var cv *dsModels.CommandValue
+
+	if reqs[0].DeviceResourceName == "randomnumber" {
+		// mimick data collection from a device
+		cv, _ = dsModels.NewInt32Value(reqs[0].DeviceResourceName, now, int32(randomInt(defMinInt32, defMaxInt32)))
+	} else if reqs[0].DeviceResourceName == "Random_Float32" {
+		cv, _ = dsModels.NewInt8Value(reqs[0].DeviceResourceName, now, int8(randomInt(defMinInt8, defMaxInt8)))
+	}
+	res[0] = cv
+	return
+}
+
+// HandleWriteCommands passes a slice of CommandRequest struct each representing
+// a ResourceOperation for a specific device resource.
+// Since the commands are actuation commands, params provide parameters for the individual
+// command.
+func (s *SimpleDriver) HandleWriteCommands(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest,
+	params []*dsModels.CommandValue) error {
+
+	if len(reqs) != 1 {
+		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; too many command requests; only one supported")
+		return err
+	}
+	if len(params) != 1 {
+		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the number of parameter is not correct; only one supported")
+		return err
+	}
+
+	s.lc.Debug(fmt.Sprintf("SimpleDriver.HandleWriteCommands: protocols: %v, resource: %v, parameters: %v", protocols, reqs[0].DeviceResourceName, params))
+	var err error
+	if s.switchButton, err = params[0].BoolValue(); err != nil {
+		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the data type of parameter should be Boolean, parameter: %s", params[0].String())
+		return err
+	}
+
+	return nil
+}
+
+// Stop the protocol-specific DS code to shutdown gracefully, or
+// if the force parameter is 'true', immediately. The driver is responsible
+// for closing any in-use channels, including the channel used to send async
+// readings (if supported).
+func (s *SimpleDriver) Stop(force bool) error {
+	// Then Logging Client might not be initialized
+	if s.lc != nil {
+		s.lc.Debug(fmt.Sprintf("SimpleDriver.Stop called: force=%v", force))
+	}
+	return nil
+}
+
+func randomInt(min int64, max int64) int64 {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Int63n(max-min) + min
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module device-simple
+
+go 1.12
+
+require (
+	github.com/edgexfoundry/device-sdk-go v1.0.0
+	github.com/edgexfoundry/go-mod-core-contracts v0.1.11
+)
